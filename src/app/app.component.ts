9a9baee8c1a708e9a8591cfed55c3f22c418507e
Otto Chamo <19001395@galileo.edu>
2022-04-29 18:39:17 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = '19001935_ht9_faw';
+}
